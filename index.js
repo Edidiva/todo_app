1b287c55db6c7c3fc39f964eff7ad5adb3255f66
Edidiva <edikanakpan5@gmail.com>
2023-03-20 23:28:51 +0100

    update

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-
+//to add item
+    function newItem() {
+    var li = document.createElement("li");
+    var inputValue = document.getElementById("myInput").value;
+    var t = document.createTextNode(inputValue);
+    li.appendChild(t);
+    if (inputValue === '') {
+    alert("You must write something!");
+    } else {
+    document.getElementById("listParent").appendChild(li);
+    }    
+}
